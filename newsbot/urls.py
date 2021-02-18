d1a71e762bf4b54f063ee949fc91b002a7d2c676
Andy Yang <andy.nba85@gmail.com>
2021-02-18 23:06:12 +0800

    'test5'

--- newsbot/urls.py
+++ newsbot/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from newsbot import views
+#from newsbot import views
+from .views import callback
+from django.conf.urls import url
 
 urlpatterns = [
-    path('callback', views.callback)
+    #path('callback', views.callback)
+    url(r'^$',callback,name=callback)
 ]
