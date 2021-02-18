4c5f03f6697f0a923fbd3ded77f34fa1f9293bf3
Andy Yang <andy.nba85@gmail.com>
2021-02-18 17:32:09 +0800

    'test'

--- newsbot/views.py
+++ newsbot/views.py
@@ -36,6 +36,28 @@ def  technews ():
 
 @csrf_exempt
 def callback(request):
+if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)  # 傳入的事件
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):  # 如果有訊息事件
+                line_bot_api.reply_message(  # 回復傳入的訊息文字
+                    event.reply_token,
+                    TextSendMessage(text=event.message.text)
+                )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
+
+'''
     if request.method == 'POST':
         #先設定一個要回傳的message空集合
         message=[]
@@ -131,4 +153,4 @@ def callback(request):
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
-                
+'''
